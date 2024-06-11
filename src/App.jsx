a3a4f4d3edc72b4fa72cef5a0a632ce701d28d7b
Gagan Chauhan <gagan2020chauhan@gmail.com>
2024-06-11 11:45:33 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Navigate, Route, Routes } from "react-router-dom";
-import axios from 'axios';
+import axios from "axios";
 
 import HomePage from "./pages/home/HomePage";
 import LoginPage from "./pages/auth/login/LoginPage";
@@ -22,50 +22,68 @@ import ResetPassword from "./pages/reset-password/ResetPassword";
 axios.defaults.withCredentials = true;
 
 function App() {
-	const { authUser } = useSelector(store => store.user);
-	const [isLoading, setIsLoading] = useState(false);
-	const dispatch = useDispatch();
+  const { authUser } = useSelector((store) => store.user);
+  const [isLoading, setIsLoading] = useState(false);
+  const dispatch = useDispatch();
 
-	useEffect(() => {
-		setIsLoading(true);
-		const fetchAuthUserData = async () => {
-			try {
-				const res = await axios.get(`${authAPI}/me`);
-				dispatch(setAuthUser(res.data));
-			} catch (err) {
-				console.log(err);
-			} finally {
-				setIsLoading(false);
-			}
-		}
+  useEffect(() => {
+    setIsLoading(true);
+    const fetchAuthUserData = async () => {
+      try {
+        const res = await axios.get(`${authAPI}/me`);
+        dispatch(setAuthUser(res.data));
+      } catch (err) {
+        console.log(err);
+      } finally {
+        setIsLoading(false);
+      }
+    };
 
-		fetchAuthUserData();
-	}, []);
+    fetchAuthUserData();
+  }, []);
 
-	if (isLoading) {
-		return (
-			<div className='h-screen flex justify-center items-center'>
-				<LoadingSpinner size='lg' />
-			</div>
-		);
-	}
+  if (isLoading) {
+    return (
+      <div className="h-screen flex justify-center items-center">
+        <LoadingSpinner size="lg" />
+      </div>
+    );
+  }
 
-	return (
-		<div className='flex max-w-6xl mx-auto'>
-			{authUser && <Sidebar />}
-			<Routes>
-				<Route path='/' element={authUser ? <HomePage /> : <Navigate to='/login' />} />
-				<Route path='/login' element={!authUser ? <LoginPage /> : <Navigate to='/' />} />
-				<Route path='/register' element={!authUser ? <RegisterPage /> : <Navigate to='/' />} />
-				<Route path='/forget-password' element={!authUser ? <ForgetPassword /> : <Navigate to='/' />} />
-				<Route path='/reset-password/:token' element={!authUser ? <ResetPassword /> : <Navigate to='/' />} />
-				<Route path='/notifications' element={authUser ? <NotificationPage /> : <Navigate to='/login' />} />
-				<Route path='/profile/:username' element={authUser ? <ProfilePage /> : <Navigate to='/login' />} />
-			</Routes>
-			{authUser && <RightPanel />}
-			<Toaster />
-		</div>
-	);
+  return (
+    <div className="flex max-w-6xl mx-auto">
+      {authUser && <Sidebar />}
+      <Routes>
+        <Route
+          path="/"
+          element={authUser ? <HomePage /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/login"
+          element={!authUser ? <LoginPage /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/register"
+          element={!authUser ? <RegisterPage /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/forget-password"
+          element={!authUser ? <ForgetPassword /> : <Navigate to="/" />}
+        />
+        <Route path="/reset-password/:token" element={<ResetPassword />} />
+        <Route
+          path="/notifications"
+          element={authUser ? <NotificationPage /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/profile/:username"
+          element={authUser ? <ProfilePage /> : <Navigate to="/login" />}
+        />
+      </Routes>
+      {authUser && <RightPanel />}
+      <Toaster />
+    </div>
+  );
 }
 
 export default App;
